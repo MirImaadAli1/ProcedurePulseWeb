9e136f5de07b972a4ac93b6bca785521f0a081e7
sum-2000 <114131022+sum-2000@users.noreply.github.com>
2024-09-05 22:11:00 +0400

    Update caughtUp.js

--- src/components/States/caughtUp.js
+++ src/components/States/caughtUp.js
@@ -10,7 +10,7 @@ const CaughtUp = () => {
       alignItems="center"
       justifyContent="center"
       height="100%" // Ensure the Box takes the full height of its parent
-    >{
+    >
       <MdCheckCircle size={48} color="#4CAF50" />
       <Typography variant="h6" mt={2}>
         You're all caught up!
